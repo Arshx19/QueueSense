0d418ab7656cc729297757161d29c79014d49074
Arsh Jain <arshjain2605@gmail.com>
2026-03-25 11:51:57 +0530

    updated auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,3 +4,10 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+router.post('/register', async(req,res)=>{
+    const {name,email,password} = req.body;
+    const hashed = await bcrypt.hash(password,10);
+    const user = ({user,email,password:hashed});
+    await user.save();
+    res.join({ message: 'User registered successfully' });
+})
