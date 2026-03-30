3c457330c5c961a4a4d04b98ec337693c20982cd
Arsh Jain <arshjain2605@gmail.com>
2026-03-31 02:25:17 +0530

    added settings UI and routing (frontend)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Register from "./pages/Register";
+import Settings from "./pages/settings";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/settings" element={<Settings/>}/>
       </Routes>
     </Router>
   );
